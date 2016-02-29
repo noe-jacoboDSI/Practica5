71ebd75c09a59c58df19696e5bcac737e25e9864
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2016-02-29 17:48:40 +0000

    practica OOP en JS

---
+++ README.md
@@ -0,0 +1,17 @@
+# Práctica OOP en JS
+
+Este repositorio aloja la práctica de conversión de tipos, realizada con html, css y javascript, usando POO y haciendo uso de la herencia en javascript.
+
+
+**Repositorio GitHuB**
+
+* [Repositorio]()
+
+**Página de la práctica**
+
+* [Web]()
+
+**Página del autor**
+
+* [Web Author]()
+
---
+++ assets/js/conversor.js
@@ -0,0 +1,59 @@
+(function(exports) {
+  "use strict";
+
+  function Medida(valor,tipo)  
+  {
+    /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
+    /* ademas de new Medida(45.2, "Km") */
+  }
+  
+  function Temperatura(valor,tipo)
+  {
+    /* tipo es opcional. Debería admitir new Medida("45.2 F") */
+  }
+  
+  function Celsius(valor)
+  {
+  }
+  
+  function Farenheit(valor)
+  {
+  }
+  
+  exports.Temperatura = Temperatura;
+  exports.Celsius = Celsius;
+  exports.Farenheit = Farenheit;
+
+  exports.convertir = function() {
+    var valor     = document.getElementById('convert').value,
+        elemento  = document.getElementById('converted'),
+        /* Extienda la RegeExp a la especificación. use una XRegExp */
+        regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i,
+        valor     = valor.match(regexp);
+    
+    if (valor) {
+      var numero = valor[1],
+          tipo   = valor[2].toLowerCase();
+      
+      numero = parseFloat(numero);
+      console.log("Valor: " + numero + ", Tipo: " + tipo);
+      
+      switch (tipo) {
+        case 'c':
+          var celsius = new Celsius(numero);
+          elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
+          break;
+        case 'f':
+          var farenheit = new Farenheit(numero);
+          elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
+          break;
+        
+        default:
+          /* rellene este código */
+      }
+    }
+    else
+      elemento.innerHTML = "";
+  }
+  
+})(this);
