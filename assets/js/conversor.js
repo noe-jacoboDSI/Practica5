e7c3718187dfdd63d7cd5f7a75b7d120bccfdd2e
alu0100836059 <alu0100836059@ull.edu.es>
2016-03-02 00:18:06 +0000

    Probando clases y herencia

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,43 +1,50 @@
 (function(exports) {
   "use strict";
 
-  function Medida(valor,tipo)  
+  function Medida(valor,tipo)
   {
+    this.valor == valor || this.tipo == tipo;
     /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
     /* ademas de new Medida(45.2, "Km") */
   }
-  
+
   function Temperatura(valor,tipo)
   {
+    Medida.call(this, valor, tipo);
+    Temperatura.prototype = new Medida();
     /* tipo es opcional. Debería admitir new Medida("45.2 F") */
   }
-  
+
   function Celsius(valor)
   {
+    Temperatura.call(this,valor);
+    Celsius.prototype = new Temperatura;
   }
-  
+
   function Farenheit(valor)
   {
+    Temperatura.call(this,valor);
+    Farenheit.prototype = new Temperatura;
   }
-  
+
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
 
   exports.convertir = function() {
-    var valor     = document.getElementById('convert').value,
+        var valor = document.getElementById('convert').value,
         elemento  = document.getElementById('converted'),
         /* Extienda la RegeExp a la especificación. use una XRegExp */
         regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i,
         valor     = valor.match(regexp);
-    
+
     if (valor) {
       var numero = valor[1],
           tipo   = valor[2].toLowerCase();
-      
+
       numero = parseFloat(numero);
       console.log("Valor: " + numero + ", Tipo: " + tipo);
-      
+
       switch (tipo) {
         case 'c':
           var celsius = new Celsius(numero);
@@ -47,7 +54,7 @@
           var farenheit = new Farenheit(numero);
           elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
           break;
-        
+
         default:
           /* rellene este código */
       }
@@ -55,5 +62,5 @@
     else
       elemento.innerHTML = "";
   }
-  
+
 })(this);
