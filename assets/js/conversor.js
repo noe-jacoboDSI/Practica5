e6554f8b4082f206aa97b71ab5ce00b9d0be1ddc
alu0100622492 <alu0100622492@ull.edu.es>
2016-03-05 20:30:51 +0000

    Modificada un poco la estrucutura html

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -88,76 +88,72 @@
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
   exports.Kelvin = Kelvin;
+  exports.Metros = Metros;
 
   exports.convertir = function() {
         //var valor = document.getElementById('convert').value,
-        var elemento  = document.getElementById('converted');
+  var elemento  = document.getElementById('converted');
         /* Extienda la RegeExp a la especificación. use una XRegExp */
-    console.log("Valor antes del if: " +valor);
+  console.log("Valor antes del if: " +valor);
 
-      var  regexp = XRegExp('(?<expresion>  ([-+]?[0-9]+(?:\.[0-9]+)?(?:e[+-]?[0-9]+)?)[ ]*([cCfFkKMm])((e|el|els|elsi|elsiu|elsius)|(a|ar|are|aren|arenh|arenhe|arenhei|arenheit)|(e|el|elv|elvi|elvin)|(e|etr|etro|etros))?) # expresion \n (?<opcional> [ ]*([Tt][oO])?[ ]*)   #opcional  \n (?<medida_destino>   [ ]*[CcKkPp])   #medida_destino ', 'x');
+  var  regexp = XRegExp('(?<expresion>  ([-+]?[0-9]+(?:\.[0-9]+)?(?:e[+-]?[0-9]+)?)[ ]*([cCfFkKMm])((e|el|els|elsi|elsiu|elsius)|(a|ar|are|aren|arenh|arenhe|arenhei|arenheit)|(e|el|elv|elvi|elvin)|(e|etr|etro|etros))?) # expresion \n (?<opcional> [ ]*([Tt][oO])?[ ]*)   #opcional  \n (?<medida_destino>   [ ]*[CcKkPp])   #medida_destino ', 'x');
 
+  //regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([fFkKcC])(e|el|els|elsi|elsiu|elsiu|elsius|a|ar|are|aren|arenh|arenhe|arenhei|arenheit|elv|elvi|elvin)?\s* (to|To|tO|TO)?\s* ([cCfFkK])$/;
+  var valor = XRegExp.exec(convert.value, regexp);
 
+  console.log("Valor antes del if: " +valor);
 
-        //regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([fFkKcC])(e|el|els|elsi|elsiu|elsiu|elsius|a|ar|are|aren|arenh|arenhe|arenhei|arenheit|elv|elvi|elvin)?\s* (to|To|tO|TO)?\s* ([cCfFkK])$/;
-        var valor = XRegExp.exec(convert.value, regexp);
-
-        console.log("Valor antes del if: " +valor);
-
-    if (valor) {
+  if (valor) {
       var numero = valor[2],
       tipo   = valor[3],
       nuevoTipo = valor.medida_destino,
       numero = parseFloat(numero);
       console.log("Valor: " + numero + ", Tipo: " + tipo, "nuevoTipo: " +nuevoTipo);
 
-
-
       switch (tipo) {
-        case 'c':
-          var celsius = new Celsius(numero);
-          if(nuevoTipo == 'f'){
-            elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
-          }
-          else if(nuevoTipo == 'k'){
-            elemento.innerHTML = celsius.toKelvin().toFixed(2) + " Kelvin";
-          }
-          break;
-        case 'f':
-          var farenheit = new Farenheit(numero);
 
-          if(nuevoTipo == 'c'){
-            elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " celsius";
-          }
-          else if(nuevoTipo == 'k'){
-            elemento.innerHTML = farenheit.toK().toFixed(2) + " kelvin";
-          }
-          break;
+        case 'c':
+            var celsius = new Celsius(numero);
+              if(nuevoTipo == 'f'){
+                elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
+              }
+            else if(nuevoTipo == 'k'){
+                elemento.innerHTML = celsius.toKelvin().toFixed(2) + " Kelvin";
+              }
+        break;
+
+        case 'f':
+            var farenheit = new Farenheit(numero);
+              if(nuevoTipo == 'c'){
+                elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " celsius";
+              }
+              else if(nuevoTipo == 'k'){
+                elemento.innerHTML = farenheit.toK().toFixed(2) + " kelvin";
+              }
+        break;
 
         case 'k':
-          var kelvin = new Kelvin(numero);
+            var kelvin = new Kelvin(numero);
+              if(nuevoTipo == 'c'){
+                elemento.innerHTML = kelvin.toC().toFixed(2) + " celsius";
+              }
+              else if(nuevoTipo == 'f'){
+                elemento.innerHTML = kelvin.toF().toFixed(2) + " farenheit";
+              }
+        break;
 
-          if(nuevoTipo == 'c'){
-            elemento.innerHTML = kelvin.toC().toFixed(2) + " celsius";
-          }
-          else if(nuevoTipo == 'f'){
-          elemento.innerHTML = kelvin.toF().toFixed(2) + " farenheit";
-          }
-          break;
-          case 'm':
-            var metros = new Metros(numero);
+        case 'm':
+              var metros = new Metros(numero);
+                if(nuevoTipo == 'p'){
+                  elemento.innerHTML = metros.toP().toFixed(2) + " pulgadas";
+                }
+        break;
 
-            if(nuevoTipo == 'p'){
-              elemento.innerHTML = metros.toP().toFixed(2) + " pulgadas";
-            }
-
-            break;
         default:
-        console.log ('Estamos en la parte default del metodo convertir');
+          console.log ('Estamos en la parte default del metodo convertir');
 
-      }
-    }
-    else
+     }
+  }else
       //elemento.innerHTML = "";
       console.log("Estamos en el else exterior.");
   }
